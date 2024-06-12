7cca1e8191099743d3c05360ae1354f21387f810
swe-bench <>
2024-06-12 16:18:25 -0400

    Task instance JavaScript/146

--- main.js
+++ main.js
@@ -1,52 +1,26 @@
-/*
-  Write a function which sorts the given list of integers
-  in ascending order according to the sum of their digits.
-  Note: if there are several items with similar sum of their digits,
-  order them based on their index in original list.
-
+/*Write a function that takes an array of numbers as input and returns 
+  the number of elements in the array that are greater than 10 and both 
+  first and last digits of a number are odd (1, 3, 5, 7, 9).
   For example:
-  >>> orderByPoints([1, 11, -1, -11, -12]) == [-1, -11, 1, -12, 11]
-  >>> orderByPoints([]) == []
+  specialFilter([15, -73, 14, -15]) => 1 
+  specialFilter([33, -2, -3, 45, 21, 109]) => 2
   */
-const orderByPoints = (nums) => {
-  let p = nums
-  for (let j = p.length - 2; j >= 0; j--) {
-    for (let k = 0; k <= j; k++) {
-      let m = 0
-      let n = 0
-      let h = p[k]
-      let d = p[k + 1]
-      let y = 1
-      let u = 1
-      if (h < 0) { y = -1; h = -h; }
-      if (d < 0) { u = -1; d = -d; }
-      while (h >= 10) {
-        m += h % 10;
-        h = (h - h % 10) / 10;
-      }
-      m += y * h + h
-      while (d >= 10) {
-        n += d % 10;
-        d = (d - d % 10) / 10;
-      }
-      n += u * d
-      if (m > n) {
-        let tmp = p[k]
-        p[k] = p[k + 1]
-        p[k + 1] = tmp
-      }
+const specialFilter = (nums) => {
+  let p = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 10) { continue }
+    let y = nums[i].toString()
+    if (Number(y[0]) % 2 == 1 && Number(y[y.length - 1]) % 2 == 1 && Number(y[y.length - 1]) % 2 == 0) {
+      p++
     }
   }
   return p
 }
 
-const testOrderByPoints = () => {
-  console.assert(
-    JSON.stringify(orderByPoints([1, 11, -1, -11, -12])) ===
-    JSON.stringify([-1, -11, 1, -12, 11])
-  )
-  console.assert(JSON.stringify(orderByPoints([])) === JSON.stringify([]))
+const testSpecialFilter = () => {
+  console.assert(specialFilter([15, -73, 14, -15]) === 1)
+  console.assert(specialFilter([33, -2, -3, 45, 21, 109]) === 2)
 }
-testOrderByPoints()
+testSpecialFilter()
 
-module.exports = orderByPoints
+module.exports = specialFilter
