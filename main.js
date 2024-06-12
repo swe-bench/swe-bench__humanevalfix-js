c7995dfa72465eb8b263d98e4616db5fa9374fe6
swe-bench <>
2024-06-12 16:18:22 -0400

    Task instance JavaScript/110

--- main.js
+++ main.js
@@ -1,56 +1,32 @@
-/*We have an array 'arr' of N integers arr[1], arr[2], ..., arr[N].The
-  numbers in the array will be randomly ordered. Your task is to determine if
-  it is possible to get an array sorted in non-decreasing order by performing 
-  the following operation on the given array:
-      You are allowed to perform right shift operation any number of times.
-  
-  One right shift operation means shifting all elements of the array by one
-  position in the right direction. The last element of the array will be moved to
-  the starting position in the array i.e. 0th index. 
-
-  If it is possible to obtain the sorted array by performing the above operation
-  then return true else return false.
-  If the given array is empty then return true.
-
-  Note: The given list is guaranteed to have unique elements.
-
-  For Example:
-  
-  moveOneBall([3, 4, 5, 1, 2])==>true
-  Explanation: By performin 2 right shift operations, non-decreasing order can
-               be achieved for the given array.
-  moveOneBall([3, 5, 4, 1, 2])==>false
-  Explanation:It is not possible to get non-decreasing order for the given
-              array by performing any number of right shift operations.
-              
+/*In this problem, you will implement a function that takes two lists of numbers,
+  and determines whether it is possible to perform an exchange of elements
+  between them to make lst1 a list of only even numbers.
+  There is no limit on the number of exchanged elements between lst1 and lst2.
+  If it is possible to exchange elements between the lst1 and lst2 to make
+  all the elements of lst1 to be even, return "YES".
+  Otherwise, return "NO".
+  For example:
+  exchange([1, 2, 3, 4], [1, 2, 3, 4]) => "YES"
+  exchange([1, 2, 3, 4], [1, 5, 3, 4]) => "NO"
+  It is assumed that the input lists will be non-empty.
   */
-const moveOneBall = (arr) => {
-  if (arr.length == 0) { return true }
-  let k = 0
-  let len = arr.length
-  for (let i = 0; i < len; i++) {
-    let t = 1;
-    for (let j = 1; j < len; j++) {
-      if (arr[j] < arr[j - 1]) {
-        t = 0;
-        break;
-      }
-    }
-    if (t == 1) {
-      k = 1;
-      break;
-    }
-    arr.push(arr[j]);
-    arr.shift()
+const exchange = (lst1, lst2) => {
+  let k = lst1.length
+  let t = 0
+  for (let i = 0; i < lst1.length; i++) {
+    if (lst1[i] % 2 == 0) { t++ }
   }
-  if (k == 1) { return true }
-  return false
+  for (let i = 0; i < lst2.length; i++) {
+    if (lst2[i] % 2 == 0) { t++ }
+  }
+  if (k >= t) { return 'YES' }
+  return 'NO'
 }
 
-const testMoveOneBall = () => {
-  console.assert(moveOneBall([3, 4, 5, 1, 2]) === true)
-  console.assert(moveOneBall([3, 5, 4, 1, 2]) === false)
+const testExchange = () => {
+  console.assert(exchange([1, 2, 3, 4], [1, 2, 3, 4]) === 'YES')
+  console.assert(exchange([1, 2, 3, 4], [1, 5, 3, 4]) === 'NO')
 }
-testMoveOneBall()
+testExchange()
 
-module.exports = moveOneBall
+module.exports = exchange
