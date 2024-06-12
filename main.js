2fcd20b8c89a5df27e49b2a0c984d0632a88a30f
swe-bench <>
2024-06-12 16:18:24 -0400

    Task instance JavaScript/129

--- main.js
+++ main.js
@@ -1,31 +1,83 @@
 /*
-  You are given an array arr of integers and you need to return
-  sum of magnitudes of integers multiplied by product of all signs
-  of each number in the array, represented by 1, -1 or 0.
-  Note: return null for empty arr.
+  Given a grid with N rows and N columns (N >= 2) and a positive integer k, 
+  each cell of the grid contains a value. Every integer in the range [1, N * N]
+  inclusive appears exactly once on the cells of the grid.
 
-  Example:
-  >>> prodSigns([1, 2, 2, -4]) == -9
-  >>> prodSigns([0, 1]) == 0
-  >>> prodSigns([]) == null
+  You have to find the minimum path of length k in the grid. You can start
+  from any cell, and in each step you can move to any of the neighbor cells,
+  in other words, you can go to cells which share an edge with you current
+  cell.
+  Please note that a path of length k means visiting exactly k cells (not
+  necessarily distinct).
+  You CANNOT go off the grid.
+  A path A (of length k) is considered less than a path B (of length k) if
+  after making the ordered lists of the values on the cells that A and B go
+  through (let's call them lst_A and lst_B), lst_A is lexicographically less
+  than lst_B, in other words, there exist an integer index i (1 <= i <= k)
+  such that lst_A[i] < lst_B[i] and for any j (1 <= j < i) we have
+  lst_A[j] = lst_B[j].
+  It is guaranteed that the answer is unique.
+  Return an ordered list of the values on the cells that the minimum path go through.
+
+  Examples:
+
+      Input: grid = [ [1,2,3], [4,5,6], [7,8,9]], k = 3
+      Output: [1, 2, 1]
+
+      Input: grid = [ [5,9,3], [4,1,6], [7,8,2]], k = 1
+      Output: [1]
   */
-const prodSigns = (arr) => {
-  if (arr.length == 0) { return null }
-  let n = 1
-  let s = 0
-  for (let i = 0; i < arr.length; i++) {
-    s += arr[i]
-    if (arr[i] == 0) { return 0 }
-    if (arr[i] < 0) { n = -n; s -= 2 * arr[i] }
+const minPath = (grid, k) => {
+  let m = 0
+  let n = 0
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid.length; j++) {
+      if (grid[i][j] == 1) {
+        m = i;
+        n = j;
+        break;
+      }
+    }
   }
-  return s * n * 2
+  let min = grid.length * grid.length
+  if (m > 0 && grid[m - 1][n] < min) { min = grid[m][n] }
+  if (n > 0 && grid[m][n - 1] < min) { min = grid[m][n] }
+  if (m < grid.length - 1 && grid[m + 1][n] < min) { min = grid[m][n] }
+  if (n < grid.length - 1 && grid[m][n + 1] < min) { min = grid[m][n] }
+  let p = []
+  for (let i = 0; i < k; i++) {
+    if (i % 2 == 0) { p.push(1) }
+    else { p.push(min) }
+  }
+  return p
 }
 
-const testProdSigns = () => {
-  console.assert(prodSigns([1, 2, 2, -4]) === -9)
-  console.assert(prodSigns([0, 1]) === 0)
-  console.assert(prodSigns([]) === null)
+const testMinPath = () => {
+  console.assert(
+    JSON.stringify(
+      minPath(
+        [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ],
+        3
+      )
+    ) === JSON.stringify([1, 2, 1])
+  )
+  console.assert(
+    JSON.stringify(
+      minPath(
+        [
+          [5, 9, 3],
+          [4, 1, 6],
+          [7, 8, 2],
+        ],
+        1
+      )
+    ) === JSON.stringify([1])
+  )
 }
-testProdSigns()
+testMinPath()
 
-module.exports = prodSigns
+module.exports = minPath
