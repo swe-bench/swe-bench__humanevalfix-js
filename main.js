aca3a2ed576df66157038f031a61c1102372e57d
swe-bench <>
2024-06-12 16:18:24 -0400

    Task instance JavaScript/130

--- main.js
+++ main.js
@@ -1,83 +1,37 @@
-/*
-  Given a grid with N rows and N columns (N >= 2) and a positive integer k, 
-  each cell of the grid contains a value. Every integer in the range [1, N * N]
-  inclusive appears exactly once on the cells of the grid.
-
-  You have to find the minimum path of length k in the grid. You can start
-  from any cell, and in each step you can move to any of the neighbor cells,
-  in other words, you can go to cells which share an edge with you current
-  cell.
-  Please note that a path of length k means visiting exactly k cells (not
-  necessarily distinct).
-  You CANNOT go off the grid.
-  A path A (of length k) is considered less than a path B (of length k) if
-  after making the ordered lists of the values on the cells that A and B go
-  through (let's call them lst_A and lst_B), lst_A is lexicographically less
-  than lst_B, in other words, there exist an integer index i (1 <= i <= k)
-  such that lst_A[i] < lst_B[i] and for any j (1 <= j < i) we have
-  lst_A[j] = lst_B[j].
-  It is guaranteed that the answer is unique.
-  Return an ordered list of the values on the cells that the minimum path go through.
-
+/*Everyone knows Fibonacci sequence, it was studied deeply by mathematicians in 
+  the last couple centuries. However, what people don't know is Tribonacci sequence.
+  Tribonacci sequence is defined by the recurrence:
+  tri(1) = 3
+  tri(n) = 1 + n / 2, if n is even.
+  tri(n) =  tri(n - 1) + tri(n - 2) + tri(n + 1), if n is odd.
+  For example:
+  tri(2) = 1 + (2 / 2) = 2
+  tri(4) = 3
+  tri(3) = tri(2) + tri(1) + tri(4)
+         = 2 + 3 + 3 = 8 
+  You are given a non-negative integer number n, you have to a return a list of the 
+  first n + 1 numbers of the Tribonacci sequence.
   Examples:
-
-      Input: grid = [ [1,2,3], [4,5,6], [7,8,9]], k = 3
-      Output: [1, 2, 1]
-
-      Input: grid = [ [5,9,3], [4,1,6], [7,8,2]], k = 1
-      Output: [1]
+  tri(3) = [1, 3, 2, 8]
   */
-const minPath = (grid, k) => {
-  let m = 0
-  let n = 0
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid.length; j++) {
-      if (grid[i][j] == 1) {
-        m = i;
-        n = j;
-        break;
-      }
+const tri = (n) => {
+  if (n == 0) { return [1] }
+  if (n == 1) { return [1, 3] }
+  let p = [1, 3]
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 == 0) {
+      p.push(1 + i / 2)
     }
-  }
-  let min = grid.length * grid.length
-  if (m > 0 && grid[m - 1][n] < min) { min = grid[m][n] }
-  if (n > 0 && grid[m][n - 1] < min) { min = grid[m][n] }
-  if (m < grid.length - 1 && grid[m + 1][n] < min) { min = grid[m][n] }
-  if (n < grid.length - 1 && grid[m][n + 1] < min) { min = grid[m][n] }
-  let p = []
-  for (let i = 0; i < k; i++) {
-    if (i % 2 == 0) { p.push(1) }
-    else { p.push(min) }
+    else {
+      p.push(p[i - 2] + p[i - 1] + 1 + i + (i + 1) / 2)
+    }
   }
   return p
 }
 
-const testMinPath = () => {
-  console.assert(
-    JSON.stringify(
-      minPath(
-        [
-          [1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9],
-        ],
-        3
-      )
-    ) === JSON.stringify([1, 2, 1])
-  )
-  console.assert(
-    JSON.stringify(
-      minPath(
-        [
-          [5, 9, 3],
-          [4, 1, 6],
-          [7, 8, 2],
-        ],
-        1
-      )
-    ) === JSON.stringify([1])
-  )
+const testTri = () => {
+  console.assert(JSON.stringify(tri(3)) === JSON.stringify([1, 3, 2.0, 8.0]))
 }
-testMinPath()
+testTri()
 
-module.exports = minPath
+module.exports = tri
