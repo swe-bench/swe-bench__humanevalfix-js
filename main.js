fc8e3be10b1b2e7c5b64db94b5a6025c4fb98783
swe-bench <>
2024-06-12 16:18:23 -0400

    Task instance JavaScript/111

--- main.js
+++ main.js
@@ -1,32 +1,58 @@
-/*In this problem, you will implement a function that takes two lists of numbers,
-  and determines whether it is possible to perform an exchange of elements
-  between them to make lst1 a list of only even numbers.
-  There is no limit on the number of exchanged elements between lst1 and lst2.
-  If it is possible to exchange elements between the lst1 and lst2 to make
-  all the elements of lst1 to be even, return "YES".
-  Otherwise, return "NO".
-  For example:
-  exchange([1, 2, 3, 4], [1, 2, 3, 4]) => "YES"
-  exchange([1, 2, 3, 4], [1, 5, 3, 4]) => "NO"
-  It is assumed that the input lists will be non-empty.
+/*Given a string representing a space separated lowercase letters, return a dictionary
+  of the letter with the most repetition and containing the corresponding count.
+  If several letters have the same occurrence, return all of them.
+  
+  Example:
+  histogram('a b c') == {'a': 1, 'b': 1, 'c': 1}
+  histogram('a b b a') == {'a': 2, 'b': 2}
+  histogram('a b c a b') == {'a': 2, 'b': 2}
+  histogram('b b b b a') == {'b': 4}
+  histogram('') == {}
+
   */
-const exchange = (lst1, lst2) => {
-  let k = lst1.length
-  let t = 0
-  for (let i = 0; i < lst1.length; i++) {
-    if (lst1[i] % 2 == 0) { t++ }
+const histogram = (test) => {
+  let d = {}
+  let t = test.split(/\s/)
+  if (test == '') { t = [] }
+  for (m in t) {
+    if (t[m] in d) {
+      d[t[m]]++
+    }
+    else {
+      d[t[m]] = 1
+    }
   }
-  for (let i = 0; i < lst2.length; i++) {
-    if (lst2[i] % 2 == 0) { t++ }
+  s = Object.keys(d).sort(function (a, b) { return - d[a] + d[b]; });
+  if (s.length == 0) { return {} }
+  let g = d[s[0]]
+  let l = {}
+  for (let ss=1; ss<s.length; ss++) {
+    if (d[s[ss]] == g) {
+      l[s[ss]] = d[s[ss]]
+    }
   }
-  if (k >= t) { return 'YES' }
-  return 'NO'
+  return l
 }
 
-const testExchange = () => {
-  console.assert(exchange([1, 2, 3, 4], [1, 2, 3, 4]) === 'YES')
-  console.assert(exchange([1, 2, 3, 4], [1, 5, 3, 4]) === 'NO')
+const testHistogram = () => {
+  console.assert(
+    JSON.stringify(histogram('a b b a')) === JSON.stringify({ a: 2, b: 2 })
+  )
+  console.assert(
+    JSON.stringify(histogram('a b c a b')) === JSON.stringify({ a: 2, b: 2 })
+  )
+  console.assert(
+    JSON.stringify(histogram('a b c d g')) ===
+    JSON.stringify({ a: 1, b: 1, c: 1, d: 1, g: 1 })
+  )
+  console.assert(
+    JSON.stringify(histogram('a b c')) === JSON.stringify({ a: 1, b: 1, c: 1 })
+  )
+  console.assert(
+    JSON.stringify(histogram('b b b b a')) === JSON.stringify({ b: 4 })
+  )
+  console.assert(JSON.stringify(histogram('')) === JSON.stringify({}))
 }
-testExchange()
+testHistogram()
 
-module.exports = exchange
+module.exports = histogram
