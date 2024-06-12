1ccbce630f71e9c9a0d8670b243777bfec4770a8
swe-bench <>
2024-06-12 16:18:22 -0400

    Task instance JavaScript/107

--- main.js
+++ main.js
@@ -1,27 +1,53 @@
-/* Implement the function f that takes n as a parameter,
-  and returns a list of size n, such that the value of the element at index i is the factorial of i if i is even
-  or the sum of numbers from 1 to i otherwise.
-  i starts from 1.
-  the factorial of i is the multiplication of the numbers from 1 to i (1 * 2 * ... * i).
-  Example:
-  f(5) == [1, 2, 6, 24, 15]
+/*
+  Given a positive integer n, return a tuple that has the number of even and odd
+  integer palindromes that fall within the range(1, n), inclusive.
+
+  Example 1:
+
+      Input: 3
+      Output: (1, 2)
+      Explanation:
+      Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
+
+  Example 2:
+
+      Input: 12
+      Output: (4, 6)
+      Explanation:
+      Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
+
+  Note:
+      1. 1 <= n <= 10^3
+      2. returned tuple has the number of even and odd integer palindromes respectively.
   */
-const f = (n) => {
-  let f = 1
-  let p = 0
-  let k = []
+const evenOddPalindrome = (n) => {
+  let e = 0
+  let o = 0
   for (let i = 1; i <= n; i++) {
-    p += i;
-    f *= i;
-    if (f % 2 == 0) { k.push(f) }
-    else { k.push(p) }
+    let k = i.toString()
+    let p = 1
+    for (let j = 0; j < k.length; j++) {
+      if (k[j] != k[k.length - j - 1]) {
+        p = 0;
+        break;
+      }
+    }
+    if (p == 1) {
+      if (k % 2 == 1) { e++ }
+      else { o++ }
+    }
   }
-  return k
+  return (e, o)
 }
 
-const testF = () => {
-  console.assert(JSON.stringify(f(5)) === JSON.stringify([1, 2, 6, 24, 15]))
+const testEvenOddPalindrome = () => {
+  console.assert(
+    JSON.stringify(evenOddPalindrome(12)) === JSON.stringify((4, 6))
+  )
+  console.assert(
+    JSON.stringify(evenOddPalindrome(3)) === JSON.stringify((1, 2))
+  )
 }
-testF()
+testEvenOddPalindrome()
 
-module.exports = f
+module.exports = evenOddPalindrome
