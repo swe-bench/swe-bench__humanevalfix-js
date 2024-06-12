a9551ccceef74dedcbe708a2c129469194d21e68
swe-bench <>
2024-06-12 16:18:18 -0400

    Task instance JavaScript/40

--- main.js
+++ main.js
@@ -1,44 +1,34 @@
 /*
-  primeFib returns n-th number that is a Fibonacci number and it's also prime.
-  >>> primeFib(1)
-  2
-  >>> primeFib(2)
-  3
-  >>> primeFib(3)
-  5
-  >>> primeFib(4)
-  13
-  >>> primeFib(5)
-  89
+  triplesSumToZero takes a list of integers as an input.
+  it returns true if there are three distinct elements in the list that
+  sum to zero, and false otherwise.
+
+  >>> triplesSumToZero([1, 3, 5, 0])
+  false
+  >>> triplesSumToZero([1, 3, -2, 1])
+  true
+  >>> triplesSumToZero([1, 2, 3, 7])
+  false
+  >>> triplesSumToZero([2, 4, -5, 3, 9, 7])
+  true
+  >>> triplesSumToZero([1])
+  false
   */
-const primeFib = (n) => {
-  var isPrime = function (p) {
-    if (p < 2)
-      return false;
-    for (let k = 2; k < Math.min(Math.floor(Math.sqrt(p)), p); k++) {
-      if (p % k == 0)
-        return false;
-    }
-    return true;
-  }
-
-  var f = [0, 1];
-  while (true) {
-    f.push(f.at(-1) + f.at(-2));
-    if (isPrime(f.at(-1)))
-      n -= 1;
-    if (n == 0)
-      return f.at(-1);
-  }
+const triplesSumToZero = (l) => {
+  for (let i = 1; i < l.length; i++)
+    for (let j = i + 1; j < l.length; j++)
+      for (let k = j + 1; k < l.length; k++)
+        if (l[i] + l[j] + l[k] == 0)
+          return true;
+  return false;
 }
 
-const testPrimeFib = () => {
-  console.assert(primeFib(1) === 2)
-  console.assert(primeFib(2) === 3)
-  console.assert(primeFib(3) === 5)
-  console.assert(primeFib(4) === 13)
-  console.assert(primeFib(5) === 89)
+const testTriplesSumToZero = () => {
+  console.assert(triplesSumToZero([1, 3, 5, 0]) === false)
+  console.assert(triplesSumToZero([1, 3, -2, 1]) === true)
+  console.assert(triplesSumToZero([1, 2, 3, 7]) === false)
+  console.assert(triplesSumToZero([2, 4, -5, 3, 9, 7]) === true)
 }
-testPrimeFib()
+testTriplesSumToZero()
 
-module.exports = primeFib
+module.exports = triplesSumToZero
