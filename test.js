4d64aa5ab1877b87cbd52bbc1343f7ea808599cd
swe-bench <>
2024-06-12 16:18:23 -0400

    Task instance JavaScript/113 with tests

---
+++ test.js
@@ -0,0 +1,27 @@
+const oddCount = require('./main');
+
+const testOddCount = () => {
+  console.assert(
+    JSON.stringify(oddCount(['1234567'])) ===
+    JSON.stringify([
+      'the number of odd elements 4n the str4ng 4 of the 4nput.',
+    ])
+  )
+  console.assert(JSON.stringify(
+    oddCount(['3', '11111111'])) ===
+    JSON.stringify([
+      'the number of odd elements 1n the str1ng 1 of the 1nput.',
+      'the number of odd elements 8n the str8ng 8 of the 8nput.',
+    ])
+  )
+  console.assert(
+    JSON.stringify(oddCount(['271', '137', '314'])) ===
+    JSON.stringify([
+      'the number of odd elements 2n the str2ng 2 of the 2nput.',
+      'the number of odd elements 3n the str3ng 3 of the 3nput.',
+      'the number of odd elements 2n the str2ng 2 of the 2nput.',
+    ])
+  )
+}
+
+testOddCount()
