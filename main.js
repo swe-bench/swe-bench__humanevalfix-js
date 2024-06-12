ac619e74935aa47f7116d73bcdf45a3aaeebb673
swe-bench <>
2024-06-12 16:18:21 -0400

    Task instance JavaScript/87

--- main.js
+++ main.js
@@ -1,42 +1,52 @@
 /*
-  Write a function that takes a string and returns an ordered version of it.
-  Ordered version of string, is a string where all words (separated by space)
-  are replaced by a new word where all the characters arranged in
-  ascending order based on ascii value.
-  Note: You should keep the order of words and blank spaces in the sentence.
-
-  For example:
-  antiShuffle('Hi') returns 'Hi'
-  antiShuffle('hello') returns 'ehllo'
-  antiShuffle('Hello World!!!') returns 'Hello !!!Wdlor'
+  You are given a 2 dimensional data, as a nested lists,
+  which is similar to matrix, however, unlike matrices,
+  each row may contain a different number of columns.
+  Given lst, and integer x, find integers x in the list,
+  and return list of tuples, [(x1, y1), (x2, y2) ...] such that
+  each tuple is a coordinate - (row, columns), starting with 0.
+  Sort coordinates initially by rows in ascending order.
+  Also, sort coordinates of the row by columns in descending order.
+  
+  Examples:
+  getRow([
+    [1,2,3,4,5,6],
+    [1,2,3,4,1,6],
+    [1,2,3,4,5,1]
+  ], 1) == [(0, 0), (1, 4), (1, 0), (2, 5), (2, 0)]
+  getRow([], 1) == []
+  getRow([[], [1], [1, 2, 3]], 3) == [(2, 2)]
   */
-const antiShuffle = (s) => {
-  let arr = s.split(/\s/)
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      let ind = j
-      for (let k = j + 1; k < arr[i].length; k++) {
-        if (arr[i][k].charCodeAt() < arr[i][ind].charCodeAt()) {
-          ind = k
-        }
-      }
-      if (ind > j) {
-        arr[i] = arr[i].slice(0, j) + arr[i][ind] + arr[i].slice(j + 1, ind) + arr[i][j] + arr[i].slice(ind + 1, arr[i].length)
+const getRow = (lst, x) => {
+  let t = []
+  for (let i = 0; i < lst.length; i++) {
+    for (let j = lst[i].length - 1; j >= 0; j--) {
+      if (lst[i][j] == x) {
+        t.push((j, i))
       }
     }
-  }
-  let t = ''
-  for (let i = 0; i < arr.length; i++) {
-    t = t + arr[i]
   }
   return t
 }
 
-const testAntiShuffle = () => {
-  console.assert(antiShuffle('Hi') === 'Hi')
-  console.assert(antiShuffle('hello') === 'ehllo')
-  console.assert(antiShuffle('Hello World!!!') === 'Hello !!!Wdlor')
+const testGetRow = () => {
+  console.assert(
+    JSON.stringify(
+      getRow(
+        [
+          [1, 2, 3, 4, 5, 6],
+          [1, 2, 3, 4, 1, 6],
+          [1, 2, 3, 4, 5, 1],
+        ],
+        1
+      )
+    ) === JSON.stringify([(0, 0), (1, 4), (1, 0), (2, 5), (2, 0)])
+  )
+  console.assert(JSON.stringify(getRow([], 1)) === JSON.stringify([]))
+  console.assert(
+    JSON.stringify(getRow([[], [1], [1, 2, 3]], 3)) === JSON.stringify([(2, 2)])
+  )
 }
-testAntiShuffle()
+testGetRow()
 
-module.exports = antiShuffle
+module.exports = getRow
