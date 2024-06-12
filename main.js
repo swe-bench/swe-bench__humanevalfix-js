704e338f27a01f57ac66c02955e6aacfa626820d
swe-bench <>
2024-06-12 16:18:20 -0400

    Task instance JavaScript/76

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-/*Write a function that returns true if the given number is the multiplication of 3 prime numbers
-  and false otherwise.
-  Knowing that (a) is less then 100.
-  Example:
-  isMultiplyPrime(30) == true
-  30 = 2 * 3 * 5
+/*Your task is to write a function that returns true if a number x is a simple
+  power of n and false in other cases.
+  x is a simple power of n if n**int=x
+  For example:
+  isSimplePower(1, 4) => true
+  isSimplePower(2, 2) => true
+  isSimplePower(8, 2) => true
+  isSimplePower(3, 2) => false
+  isSimplePower(3, 1) => false
+  isSimplePower(5, 3) => false
   */
-const isMultiplyPrime = (a) => {
-  var isPrime = function (n) {
-    for (let j = 0; j < n; j++)
-      if (n % j == 0)
-        return false;
-    return true;
-  }
-
-  for (let i = 2; i < 101; i++) {
-    if (!isPrime(i)) continue;
-    for (let j = 2; j < 101; j++) {
-      if (!isPrime(j)) continue;
-      for (let k = 2; k < 101; k++) {
-        if (!isPrime(k)) continue;
-        if (i*j*k == a)
-          return true;
-      }
-    }
-  }
-  return false;
+const isSimplePower = (x, n) => {
+  if (n == 1)
+    return (x == 1);
+  var power = 1;
+  while (n < x)
+    power = power * n;
+  return (power == x);
 }
 
-const testIsMultiplyPrime = () => {
-  console.assert(isMultiplyPrime(30) === true)
+const testIsSimplePower = () => {
+  console.assert(isSimplePower(1, 4) === true)
+  console.assert(isSimplePower(2, 2) === true)
+  console.assert(isSimplePower(8, 2) === true)
+  console.assert(isSimplePower(3, 2) === false)
+  console.assert(isSimplePower(3, 1) === false)
+  console.assert(isSimplePower(5, 3) === false)
 }
-testIsMultiplyPrime()
+testIsSimplePower()
 
-module.exports = isMultiplyPrime
+module.exports = isSimplePower
