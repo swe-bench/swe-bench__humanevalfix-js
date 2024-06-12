d33b8bd5d526e7c714fa9d27627b7d2a0342d7cb
swe-bench <>
2024-06-12 16:18:20 -0400

    Task instance JavaScript/74

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
 /*
-  Given an array arr of integers, find the minimum number of elements that
-  need to be changed to make the array palindromic. A palindromic array is an array that
-  is read the same backwards and forwards. In one change, you can change one element to any other element.
+  Write a function that accepts two lists of strings and returns the list that has
+  total number of chars in the all strings of the list less than the other list.
 
-  For example:
-  smallestChange([1,2,3,5,4,7,9,6]) == 4
-  smallestChange([1, 2, 3, 4, 3, 2, 2]) == 1
-  smallestChange([1, 2, 3, 2, 1]) == 0
+  if the two lists have the same number of chars, return the first list.
+
+  Examples
+  totalMatch([], []) ➞ []
+  totalMatch(['hi', 'admin'], ['hI', 'Hi']) ➞ ['hI', 'Hi']
+  totalMatch(['hi', 'admin'], ['hi', 'hi', 'admin', 'project']) ➞ ['hi', 'admin']
+  totalMatch(['hi', 'admin'], ['hI', 'hi', 'hi']) ➞ ['hI', 'hi', 'hi']
+  totalMatch(['4'], ['1', '2', '3', '4', '5']) ➞ ['4']
   */
-const smallestChange = (arr) => {
-  var ans = 0;
-  for (let i = 0; i < Math.floor(arr.length / 2); i++)
-    if (ans != arr.at(-i - 1))
-      ans++;
-  return ans;
+const totalMatch = (lst1, lst2) => {
+  var l1 = lst1.reduce(((prev, item) => prev + item.length), 0);
+  var l2 = lst2.reduce(((prev, item) => prev + item.length), 0);
+  if (l1 <= l2)
+    return lst2;
+  else
+    return lst1;
 }
 
-const testSmallestChange = () => {
-  console.assert(smallestChange([1, 2, 3, 5, 4, 7, 9, 6]) === 4)
-  console.assert(smallestChange([1, 2, 3, 4, 3, 2, 2]) === 1)
-  console.assert(smallestChange([1, 2, 3, 2, 1]) === 0)
-  console.assert(smallestChange([3, 1, 1, 3]) === 0)
+const testTotalMatch = () => {
+  console.assert(JSON.stringify(totalMatch([], [])) === JSON.stringify([]))
+  console.assert(
+    JSON.stringify(
+      totalMatch(['hi', 'admin'], ['hi', 'hi', 'admin', 'project'])
+    ) === JSON.stringify(['hi', 'admin'])
+  )
+  console.assert(
+    JSON.stringify(totalMatch(['4'], ['1', '2', '3', '4', '5'])) ===
+      JSON.stringify(['4'])
+  )
+  console.assert(
+    JSON.stringify(totalMatch(['hi', 'admin'], ['hI', 'Hi'])) ===
+      JSON.stringify(['hI', 'Hi'])
+  )
+  console.assert(
+    JSON.stringify(totalMatch(['hi', 'admin'], ['hI', 'hi', 'hi'])) ===
+      JSON.stringify(['hI', 'hi', 'hi'])
+  )
 }
-testSmallestChange()
+testTotalMatch()
 
-module.exports = smallestChange
+module.exports = totalMatch
