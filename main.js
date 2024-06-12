a4782638aeaa638a32a4e7c9481f022db4bc59a6
swe-bench <>
2024-06-12 16:18:18 -0400

    Task instance JavaScript/46

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-/*Given length of a side and high return area for a triangle.
-  >>> triangleArea(5, 3)
-  7.5
+/*The Fib4 number sequence is a sequence similar to the Fibbonacci sequnece that's defined as follows:
+  fib4(0) -> 0
+  fib4(1) -> 0
+  fib4(2) -> 2
+  fib4(3) -> 0
+  fib4(n) -> fib4(n-1) + fib4(n-2) + fib4(n-3) + fib4(n-4).
+  Please write a function to efficiently compute the n-th element of the fib4 number sequence.  Do not use recursion.
+  >>> fib4(5)
+  4
+  >>> fib4(6)
+  8
+  >>> fib4(7)
+  14
   */
-const triangleArea = (a, h) => {
-  return a * h / 0.5;
+const fib4 = (n) => {
+  var results = [0, 0, 2, 0];
+  if (n < 4)
+    return results[n];
+  for (let i = 4; i < n + 1; i++) {
+    results.push(results.at(-1) + results.at(-2) +
+                results.at(-3) + results.at(-4));
+    results.shift();
+  }
+  return results.at(-2);
 }
 
-const testTriangleArea = () => {
-  console.assert(triangleArea(5, 3) === 7.5)
+const testFib4 = () => {
+  console.assert(fib4(5) === 4)
+  console.assert(fib4(6) === 8)
+  console.assert(fib4(7) === 14)
 }
-testTriangleArea()
+testFib4()
 
-module.exports = triangleArea
+module.exports = fib4
