1b728015620ace22cc45024726a06e2de28bc5e2
swe-bench <>
2024-06-12 16:18:15 -0400

    Task instance JavaScript/1

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-/* Check if in given list of numbers, are any two numbers closer to each other than
-  given threshold.
-  >>> hasCloseElements([1.0, 2.0, 3.0], 0.5)
-  false
-  >>> hasCloseElements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)
-  true
+/* Input to this function is a string containing multiple groups of nested parentheses. Your goal is to
+  separate those group into separate strings and return the list of those.
+  Separate groups are balanced (each open brace is properly closed) and not nested within each other
+  Ignore any spaces in the input string.
+  >>> separateParenGroups('( ) (( )) (( )( ))')
+  ['()', '(())', '(()())']
   */
-const hasCloseElements = (numbers, threshold) => {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      if (i != j) {
-        let distance = numbers[i] - numbers[j];
-        if (distance < threshold) {
-          return true;
-        }
+const separateParenGroups = (paren_string) => {
+  var result = [];
+  var current_string = [];
+  var current_depth = 0;
+
+  for (const c of paren_string) {
+    if (c == '(') {
+      current_depth += 1;
+      current_string.push(c);
+    } else if (c == ')') {
+      current_depth -= 1;
+      current_string.push(c);
+      if (current_depth < 0) {
+        result.push(current_string.join(''));
+        current_string = [];
       }
     }
   }
-  return false;
+
+  return result;
 }
 
-const testHasCloseElements = () => {
-  console.assert(hasCloseElements([1.0, 2.0, 3.0], 0.5) === false)
+const testSeparateParenGroups = () => {
   console.assert(
-    hasCloseElements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3) === true
+    JSON.stringify(separateParenGroups('( ) (( )) (( )( ))')) ===
+      JSON.stringify(['()', '(())', '(()())'])
   )
 }
-testHasCloseElements()
+testSeparateParenGroups()
 
-module.exports = hasCloseElements
+module.exports = separateParenGroups
