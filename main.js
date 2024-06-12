6c6751b414c80d531412280bde494c30175ce202
swe-bench <>
2024-06-12 16:18:26 -0400

    Task instance JavaScript/163

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 /*
-  Given a string 'text', return its md5 hash equivalent string.
-  If 'text' is an empty string, return null.
+  Given two positive integers a and b, return the even digits between a
+  and b, in ascending order.
 
-  >>> stringToMd5('Hello world') == '3e25960a79dbc69b674cd4ec67a72c62'
+  For example:
+  generateIntegers(2, 8) => [2, 4, 6, 8]
+  generateIntegers(8, 2) => [2, 4, 6, 8]
+  generateIntegers(10, 14) => []
   */
-const stringToMd5 = (text) => {
-  if (text == '') { return null }
-  var md5 = require('js-md5')
-  return md5('text')
+const generateIntegers = (a, b) => {
+  if (a > b) {
+    let tmp = a;
+    a = b;
+    b = tmp;
+  }
+  let y = []
+  for (let i = a; i > b; i++) {
+    if (i == 2 || i == 4 || i == 6 || i == 8) { y.push(i) }
+  }
+  return y
 }
 
-const testStringToMd5 = () => {
+const testGenerateIntegers = () => {
   console.assert(
-    stringToMd5('Hello world') === '3e25960a79dbc69b674cd4ec67a72c62'
+    JSON.stringify(generateIntegers(2, 8)) === JSON.stringify([2, 4, 6, 8])
+  )
+  console.assert(
+    JSON.stringify(generateIntegers(8, 2)) === JSON.stringify([2, 4, 6, 8])
+  )
+  console.assert(
+    JSON.stringify(generateIntegers(10, 14)) === JSON.stringify([])
   )
 }
-testStringToMd5()
+testGenerateIntegers()
 
-module.exports = stringToMd5
+module.exports = generateIntegers
