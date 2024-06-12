f20fad793e25fd0203d4ca5b136b40d0c38fd2c9
swe-bench <>
2024-06-12 16:18:21 -0400

    Task instance JavaScript/94

--- main.js
+++ main.js
@@ -1,31 +1,51 @@
-/*
-  Write a function that takes a message, and encodes in such a 
-  way that it swaps case of all letters, replaces all vowels in 
-  the message with the letter that appears 2 places ahead of that 
-  vowel in the english alphabet. 
-  Assume only letters. 
-  
+/*You are given a list of integers.
+  You need to find the largest prime value and return the sum of its digits.
+
   Examples:
-  >>> encode('test')
-  'TGST'
-  >>> encode('This is a message')
-  'tHKS KS C MGSSCGG'
+  For lst = [0,3,2,1,3,5,7,4,5,5,5,2,181,32,4,32,3,2,32,324,4,3] the output should be 10
+  For lst = [1,0,1,8,2,4597,2,1,3,40,1,2,1,2,4,2,5,1] the output should be 25
+  For lst = [1,3,1,32,5107,34,83278,109,163,23,2323,32,30,1,9,3] the output should be 13
+  For lst = [0,724,32,71,99,32,6,0,5,91,83,0,5,6] the output should be 11
+  For lst = [0,81,12,3,1,21] the output should be 3
+  For lst = [0,8,1,2,1,7] the output should be 7
   */
-const encode = (message) => {
-  let t = ''
-  for (let i = 0; i < message.length; i++) {
-    let p = message[i].charCodeAt()
-    if (p > 96) { p -= 32 }
-    else if (p!=32 && p < 96) { p += 32 }
-    if (p == 65 || p == 97 || p == 69 || p == 101 || p == 73 || p == 105 || p == 79 || p == 111 || p == 85 || p == 117) { p += 2 }
+const skjkasdkd = (lst) => {
+  let t = 0
+  for (let i = 0; i < lst.length; i++) {
+    let p = 1
+    for (let j = 2; j * j <= lst[i]; j++) {
+      if (lst[i] % j == 0) { p = 0; break }
+    }
+    if (p == 1 || lst[i] > t) { t = lst[i] }
   }
-  return t
+  let k = 0
+  while (t != 0) {
+    k += t % 10
+    t = (t - t % 10) / 10
+  }
+  return k
 }
 
-const testEncode = () => {
-  console.assert(encode('test') === 'TGST')
-  console.assert(encode('This is a message') === 'tHKS KS C MGSSCGG')
+const testSkjkasdkd = () => {
+  console.assert(
+    skjkasdkd([
+      0, 3, 2, 1, 3, 5, 7, 4, 5, 5, 5, 2, 181, 32, 4, 32, 3, 2, 32, 324, 4, 3,
+    ]) === 10
+  )
+  console.assert(
+    skjkasdkd([1, 0, 1, 8, 2, 4597, 2, 1, 3, 40, 1, 2, 1, 2, 4, 2, 5, 1]) === 25
+  )
+  console.assert(
+    skjkasdkd([
+      1, 3, 1, 32, 5107, 34, 83278, 109, 163, 23, 2323, 32, 30, 1, 9, 3,
+    ]) === 13
+  )
+  console.assert(
+    skjkasdkd([0, 724, 32, 71, 99, 32, 6, 0, 5, 91, 83, 0, 5, 6]) === 11
+  )
+  console.assert(skjkasdkd([0, 81, 12, 3, 1, 21]) === 3)
+  console.assert(skjkasdkd([0, 8, 1, 2, 1, 7]) === 7)
 }
-testEncode()
+testSkjkasdkd()
 
-module.exports = encode
+module.exports = skjkasdkd
