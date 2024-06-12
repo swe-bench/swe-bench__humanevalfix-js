5bda7aa9a8a79efe9164959bf9de8231d8f25231
swe-bench <>
2024-06-12 16:18:20 -0400

    Task instance JavaScript/75

--- main.js
+++ main.js
@@ -1,45 +1,35 @@
-/*
-  Write a function that accepts two lists of strings and returns the list that has
-  total number of chars in the all strings of the list less than the other list.
-
-  if the two lists have the same number of chars, return the first list.
-
-  Examples
-  totalMatch([], []) ➞ []
-  totalMatch(['hi', 'admin'], ['hI', 'Hi']) ➞ ['hI', 'Hi']
-  totalMatch(['hi', 'admin'], ['hi', 'hi', 'admin', 'project']) ➞ ['hi', 'admin']
-  totalMatch(['hi', 'admin'], ['hI', 'hi', 'hi']) ➞ ['hI', 'hi', 'hi']
-  totalMatch(['4'], ['1', '2', '3', '4', '5']) ➞ ['4']
+/*Write a function that returns true if the given number is the multiplication of 3 prime numbers
+  and false otherwise.
+  Knowing that (a) is less then 100.
+  Example:
+  isMultiplyPrime(30) == true
+  30 = 2 * 3 * 5
   */
-const totalMatch = (lst1, lst2) => {
-  var l1 = lst1.reduce(((prev, item) => prev + item.length), 0);
-  var l2 = lst2.reduce(((prev, item) => prev + item.length), 0);
-  if (l1 <= l2)
-    return lst2;
-  else
-    return lst1;
+const isMultiplyPrime = (a) => {
+  var isPrime = function (n) {
+    for (let j = 0; j < n; j++)
+      if (n % j == 0)
+        return false;
+    return true;
+  }
+
+  for (let i = 2; i < 101; i++) {
+    if (!isPrime(i)) continue;
+    for (let j = 2; j < 101; j++) {
+      if (!isPrime(j)) continue;
+      for (let k = 2; k < 101; k++) {
+        if (!isPrime(k)) continue;
+        if (i*j*k == a)
+          return true;
+      }
+    }
+  }
+  return false;
 }
 
-const testTotalMatch = () => {
-  console.assert(JSON.stringify(totalMatch([], [])) === JSON.stringify([]))
-  console.assert(
-    JSON.stringify(
-      totalMatch(['hi', 'admin'], ['hi', 'hi', 'admin', 'project'])
-    ) === JSON.stringify(['hi', 'admin'])
-  )
-  console.assert(
-    JSON.stringify(totalMatch(['4'], ['1', '2', '3', '4', '5'])) ===
-      JSON.stringify(['4'])
-  )
-  console.assert(
-    JSON.stringify(totalMatch(['hi', 'admin'], ['hI', 'Hi'])) ===
-      JSON.stringify(['hI', 'Hi'])
-  )
-  console.assert(
-    JSON.stringify(totalMatch(['hi', 'admin'], ['hI', 'hi', 'hi'])) ===
-      JSON.stringify(['hI', 'hi', 'hi'])
-  )
+const testIsMultiplyPrime = () => {
+  console.assert(isMultiplyPrime(30) === true)
 }
-testTotalMatch()
+testIsMultiplyPrime()
 
-module.exports = totalMatch
+module.exports = isMultiplyPrime
