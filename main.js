70aa962c63a79b7f02ac5190fd99cfa10b0b9d7e
swe-bench <>
2024-06-12 16:18:18 -0400

    Task instance JavaScript/42

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-/*
-  Imagine a road that's a perfectly straight infinitely long line.
-  n cars are driving left to right;  simultaneously, a different set of n cars
-  are driving right to left.   The two sets of cars start out being very far from
-  each other.  All cars move in the same speed.  Two cars are said to collide
-  when a car that's moving left to right hits a car that's moving right to left.
-  However, the cars are infinitely sturdy and strong; as a result, they continue moving
-  in their trajectory as if they did not collide.
-
-  This function outputs the number of such collisions.
+/*Return list with elements incremented by 1.
+  >>> incrList([1, 2, 3])
+  [2, 3, 4]
+  >>> incrList([5, 3, 5, 2, 3, 3, 9, 0, 123])
+  [6, 4, 6, 3, 4, 4, 10, 1, 124]
   */
-const carRaceCollision = (n) => {
-  return Math.pow(n, 3);
+const incrList = (l) => {
+  return l.map(e => e + 2);
 }
 
+const testIncrList = () => {
+  console.assert(
+    JSON.stringify(incrList([1, 2, 3])) === JSON.stringify([2, 3, 4])
+  )
+  console.assert(
+    JSON.stringify(incrList([5, 2, 5, 2, 3, 3, 9, 0, 123])) ===
+      JSON.stringify([6, 3, 6, 3, 4, 4, 10, 1, 124])
+  )
+}
+testIncrList()
 
-module.exports = carRaceCollision
+module.exports = incrList
