1cbadbf04594576c49bf31778cf1ac09f6c63022
swe-bench <>
2024-06-12 16:18:18 -0400

    Task instance JavaScript/43

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-/*Return list with elements incremented by 1.
-  >>> incrList([1, 2, 3])
-  [2, 3, 4]
-  >>> incrList([5, 3, 5, 2, 3, 3, 9, 0, 123])
-  [6, 4, 6, 3, 4, 4, 10, 1, 124]
+/*
+  pairsSumToZero takes a list of integers as an input.
+  it returns true if there are two distinct elements in the list that
+  sum to zero, and false otherwise.
+  >>> pairsSumToZero([1, 3, 5, 0])
+  false
+  >>> pairsSumToZero([1, 3, -2, 1])
+  false
+  >>> pairsSumToZero([1, 2, 3, 7])
+  false
+  >>> pairsSumToZero([2, 4, -5, 3, 5, 7])
+  true
+  >>> pairsSumToZero([1])
+  false
   */
-const incrList = (l) => {
-  return l.map(e => e + 2);
+const pairsSumToZero = (l) => {
+  for (let i = 0; i < l.length; i++)
+    for (let j = i; j < l.length; j++)
+      if (l[i] + l[j] == 0)
+        return true;
+  return false;
 }
 
-const testIncrList = () => {
-  console.assert(
-    JSON.stringify(incrList([1, 2, 3])) === JSON.stringify([2, 3, 4])
-  )
-  console.assert(
-    JSON.stringify(incrList([5, 2, 5, 2, 3, 3, 9, 0, 123])) ===
-      JSON.stringify([6, 3, 6, 3, 4, 4, 10, 1, 124])
-  )
+const testPairsSumToZero = () => {
+  console.assert(pairsSumToZero([1, 3, 5, 0]) === false)
+  console.assert(pairsSumToZero([1, 3, -2, 1]) === false)
+  console.assert(pairsSumToZero([1, 2, 3, 7]) === false)
+  console.assert(pairsSumToZero([2, 4, -5, 3, 5, 7]) === true)
 }
-testIncrList()
+testPairsSumToZero()
 
-module.exports = incrList
+module.exports = pairsSumToZero
