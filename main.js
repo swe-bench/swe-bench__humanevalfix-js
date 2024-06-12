2ca40a7b03ecdca516ed84d6fe4e1b0304284f07
swe-bench <>
2024-06-12 16:18:19 -0400

    Task instance JavaScript/51

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 /*
-  returns encoded string by shifting every character by 5 in the alphabet.
+  removeVowels is a function that takes string and returns string without vowels.
+  >>> removeVowels('')
+  ''
+  >>> removeVowels("abcdef\nghijklm")
+  'bcdf\nghjklm'
+  >>> removeVowels('abcdef')
+  'bcdf'
+  >>> removeVowels('aaaaa')
+  ''
+  >>> removeVowels('aaBAA')
+  'B'
+  >>> removeVowels('zbcd')
+  'zbcd'
   */
-const encodeShift = (s) => {
-  return s.split("").map(ch => String.fromCharCode(
-    ((ch.charCodeAt(0) + 5 - "a".charCodeAt(0)) % 26) + "a".charCodeAt(0)
-  )).join("");
+const removeVowels = (text) => {
+  return text.split("")
+             .filter(s => !["a", "e", "i", "o", "u", "w", "y"]
+                      .includes(s.toLowerCase())
+                    )
+             .join("")
 }
 
-/*
-  takes as input string encoded with encode_shift function. Returns decoded string.
-  */
-const decodeShift = (s) => {
-  return s.split("").map(ch => String.fromCharCode(
-    ((ch.charCodeAt(0) - 5 + 26 - "a".charCodeAt(0)) % 26) + ch.charCodeAt(0)
-  )).join("");
+const testRemoveVowels = () => {
+  console.assert(removeVowels('') === '')
+  console.assert(removeVowels('abcdef\nghijklm') === 'bcdf\nghjklm')
+  console.assert(removeVowels('abcdef') === 'bcdf')
+  console.assert(removeVowels('aaaaa') === '')
+  console.assert(removeVowels('aaBAA') === 'B')
+  console.assert(removeVowels('zbcd') === 'zbcd')
 }
+testRemoveVowels()
 
-
-module.exports = decodeShift
+module.exports = removeVowels
