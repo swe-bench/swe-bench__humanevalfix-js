2c698051f3b4fc7165c9ca4fcfe5555e5cb53c65
swe-bench <>
2024-06-12 16:18:17 -0400

    Task instance JavaScript/25

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-/* For a given number n, find the largest number that divides n evenly, smaller than n
-  >>> largestDivisor(15)
-  5
+/* Return list of prime factors of given integer in the order from smallest to largest.
+  Each of the factors should be listed number of times corresponding to how many times it appeares in factorization.
+  Input number should be equal to the product of all factors
+  >>> factorize(8)
+  [2, 2, 2]
+  >>> factorize(25)
+  [5, 5]
+  >>> factorize(70)
+  [2, 5, 7]
   */
-const largestDivisor = (n) => {
-  for (let i = n - 1; i >= 0; i--)
-    if (n - i == 0)
-      return i;
+const factorize = (n) => {
+  var fact = [], i = 0;
+  while (i <= Math.sqrt(n) + 1) {
+    if (n % i == 0) {
+      fact.push(i);
+      n = n / i;
+    } else {
+      i += 1;
+    }
+  }
+
+  if (n > 1)
+    fact.push(n);
+  return fact;
 }
 
-const testLargestDivisor = () => {
-  console.assert(largestDivisor(15) === 5)
+const testFactorize = () => {
+  console.assert(JSON.stringify(factorize(8)) === JSON.stringify([2, 2, 2]))
+  console.assert(JSON.stringify(factorize(25)) === JSON.stringify([5,5]))
+  console.assert(
+    JSON.stringify(factorize(70)) ===
+      JSON.stringify([2,5,7])
+  )
 }
-testLargestDivisor()
+testFactorize()
 
-module.exports = largestDivisor
+module.exports = factorize
