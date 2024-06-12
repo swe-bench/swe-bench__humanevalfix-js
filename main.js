96c33bb65a1a34e8f92443d8bde0c1fa31db0fba
swe-bench <>
2024-06-12 16:18:26 -0400

    Task instance JavaScript/161

--- main.js
+++ main.js
@@ -1,81 +1,37 @@
-/*
-  Given two lists operator, and operand. The first list has basic algebra operations, and 
-  the second list is a list of integers. Use the two given lists to build the algebric 
-  expression and return the evaluation of this expression.
-
-  The basic algebra operations:
-  Addition ( + ) 
-  Subtraction ( - ) 
-  Multiplication ( * ) 
-  Floor division ( // ) 
-  Exponentiation ( ** ) 
-
-  Example:
-  operator['+', '*', '-']
-  array = [2, 3, 4, 5]
-  result = 2 + 3 * 4 - 5
-  => result = 9
-
-  Note:
-      The length of operator list is equal to the length of operand list minus one.
-      Operand is a list of of non-negative integers.
-      Operator list has at least one operator, and operand list has at least two operands.
-
+/*You are given a string s.
+  if s[i] is a letter, reverse its case from lower to upper or vise versa, 
+  otherwise keep it as it is.
+  If the string contains no letters, reverse the string.
+  The function should return the resulted string.
+  Examples
+  solve("1234") = "4321"
+  solve("ab") = "AB"
+  solve("#a@C") = "#A@c"
   */
-const doAlgebra = (operator, operand) => {
-  while (operator.length > 0) {
-    let y = 0
-    for (let i = operator.length - 1; i >= 0; i--) {
-      if (operator[i] == '**') {
-        let u = operand[i]
-        while (operand[i + 1] > 1) {
-          operand[i + 1]--;
-          operand[i] *= u;
-        }
-        operand.splice(i + 1, 1)
-        operator.splice(i, 1)
-        y = 1;
-        break;
-      }
+const solve = (s) => {
+  let t = 0
+  let p = ''
+  for (let i = 0; i < s.length; i++) {
+    let y = s[i].charCodeAt()
+    if (y >= 65 && y <= 90) {
+      y += 32;
+      t = 1;
     }
-    if (y == 1) { continue }
-    for (let i = 0; i < operator.length; i++) {
-      if (operator[i] == '*') {
-        operand[i] *= operand[i + 1]
-        operand.splice(i + 1, 1)
-        operator.splice(i, 1)
-        y = 1;
-        break;
-      }
-      else if (operator[i] == '//') {
-        operand[i] = (operand[i + 1] - operand[i] % operand[i + 1]) / operand[i + 1]
-        operand.splice(i + 1, 1)
-        operator.splice(i, 1)
-        y = 1;
-        break;
-      }
-    }
-    if (y == 1) { continue }
-    for (let i = 0; i < operator.length; i++) {
-      if (operator[i] == '+') {
-        operand[i] += operand[i + 1]
-        operand.splice(i + 1, 1)
-        operator.splice(i, 1)
-        y = 1;
-        break;
-      }
-      else if (operator[i] == '-') {
-        operand[i] -= operand[i + 1]
-        operand.splice(i + 1, 1)
-        operator.splice(i, 1)
-        y = 1;
-        break;
-      }
-    }
-    if (y == 1) { continue }
+    p += String.fromCharCode(y)
   }
-  return operand[0]
+  if (t == 1) { return p }
+  let u = ''
+  for (let i = 0; i < p.length; i++) {
+    u += p[p.length - i - 1]
+  }
+  return u
 }
 
+const testSolve = () => {
+  console.assert(solve('1234') === '4321')
+  console.assert(solve('ab') === 'AB')
+  console.assert(solve('#a@C') === '#A@c')
+}
+testSolve()
 
-module.exports = doAlgebra
+module.exports = solve
--- test.js
+++
@@ -1,9 +0,0 @@
-const doAlgebra = require('./main');
-
-const testDoAlgebra = () => {
-  console.assert(doAlgebra(['**', '*', '+'], [2, 3, 4, 5]) === 37)
-  console.assert(doAlgebra(['+', '*', '-'], [2, 3, 4, 5]) === 9)
-  console.assert(doAlgebra(['//', '*'], [7, 3, 4]) === 8)
-}
-
-testDoAlgebra()
