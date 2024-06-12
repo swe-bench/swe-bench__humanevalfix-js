71e62174b0c9e40bdc5e4b7c713305d6da784b9f
swe-bench <>
2024-06-12 16:18:23 -0400

    Task instance JavaScript/114

--- main.js
+++ main.js
@@ -1,43 +1,28 @@
-/*Given a list of strings, where each string consists of only digits, return a list.
-  Each element i of the output should be "the number of odd elements in the
-  string i of the input." where all the i's should be replaced by the number
-  of odd digits in the i'th string of the input.
-
-  >>> oddCount(['1234567'])
-  ["the number of odd elements 4n the str4ng 4 of the 4nput."]
-  >>> oddCount(['3',"11111111"])
-  ["the number of odd elements 1n the str1ng 1 of the 1nput.",
-   "the number of odd elements 8n the str8ng 8 of the 8nput."]
+/*
+  Given an array of integers nums, find the minimum sum of any non-empty sub-array
+  of nums.
+  Example
+  minSubArraySum([2, 3, 4, 1, 2, 4]) == 1
+  minSubArraySum([-1, -2, -3]) == -6
   */
-const oddCount = (lst) => {
-  let d = []
-  for (let i = 0; i < lst.length; i++) {
-    let p = 0;
-    let h = lst[i].length
-    for (let j = 0; j < h; j++) {
-      if (lst[i][j].charCodeAt() % 2 == 1) { p++ }
+const minSubArraySum = (nums) => {
+  let min = Math.min(nums)
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j <= nums.length; j++) {
+      let s = 0;
+      for (let k = i; k < j; k++) {
+        s += nums[k]
+      }
+      if (s < min) { min = s }
     }
-    p = p.toString()
-    d.push('the number of odd elements ' + p + 'n the str' + p + 'ng ' + p + ' of '  p + ' the ' + p + 'nput.')
   }
-  return d
+  return min
 }
 
-const testOddCount = () => {
-  console.assert(
-    JSON.stringify(oddCount(['1234567'])) ===
-    JSON.stringify([
-      'the number of odd elements 4n the str4ng 4 of the 4nput.',
-    ])
-  )
-  console.assert(JSON.stringify(
-    oddCount(['3', '11111111'])) ===
-    JSON.stringify([
-      'the number of odd elements 1n the str1ng 1 of the 1nput.',
-      'the number of odd elements 8n the str8ng 8 of the 8nput.',
-    ])
-  )
+const testMinSubArraySum = () => {
+  console.assert(minSubArraySum([2, 3, 4, 1, 2, 4]) === 1)
+  console.assert(minSubArraySum([-1, -2, -3]) === -6)
 }
-testOddCount()
+testMinSubArraySum()
 
-module.exports = oddCount
+module.exports = minSubarraySum
