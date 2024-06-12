5400210479e41ccd1898af06964a133601f4cdac
swe-bench <>
2024-06-12 16:18:19 -0400

    Task instance JavaScript/62

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
-/* brackets is a string of "(" and ")".
-  return true if every opening bracket has a corresponding closing bracket.
-
-  >>> correctBracketing("(")
-  false
-  >>> correctBracketing("()")
-  true
-  >>> correctBracketing("(()())")
-  true
-  >>> correctBracketing(")(()")
-  false
+/* xs represent coefficients of a polynomial.
+  xs[0] + xs[1] * x + xs[2] * x^2 + ....
+   Return derivative of this polynomial in the same form.
+  >>> derivative([3, 1, 2, 4, 5])
+  [1, 4, 12, 20]
+  >>> derivative([1, 2, 3])
+  [2, 6]
   */
-const correctBracketing = (brackets) => {
-  var depth = 0;
-  for (const b of brackets) {
-    if (b == "(")
-      depth += 1;
-    else
-      depth -= 1;
-    if (depth < 0)
-      return true;
-  }
-  return depth == 0;
+const derivative = (xs) => {
+  return xs.map((x, i) => x * i);
 }
 
-const testCorrectBracketing = () => {
-  console.assert(correctBracketing('()') === true)
-  console.assert(correctBracketing('(()())') === true)
-  console.assert(correctBracketing(')(()') === false)
-  console.assert(correctBracketing('(') === false)
+const testDerivative = () => {
+  console.assert(
+    JSON.stringify(derivative([3, 1, 2, 4, 5])) ===
+      JSON.stringify([1, 4, 12, 20])
+  )
+  console.assert(
+    JSON.stringify(derivative([1, 2, 3])) === JSON.stringify([2, 6])
+  )
 }
-testCorrectBracketing()
+testDerivative()
 
-module.exports = correctBracketing
+module.exports = derivative
