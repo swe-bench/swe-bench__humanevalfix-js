3a37a26d5833f8505e16b21b3753e9c14f01ddef
swe-bench <>
2024-06-12 16:18:15 -0400

    Task instance JavaScript/10

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-/* From a given list of integers, generate a list of rolling maximum element found until given moment
-  in the sequence.
-  >>> rollingMax([1, 2, 3, 2, 3, 4, 2])
-  [1, 2, 3, 3, 3, 4, 4]
+/* Test if gniven strig is a palindrome */
+const isPalindrome = (string) => {
+  return string == string.split('').reverse().join('');
+}
+
+/* Find the shortest palindrome that begins with a supplied string.
+  Algorithm idea is simple:
+  - Find the longest postfix of supplied string that is a palindrome.
+  - Append to the end of the string reverse of a string prefix that comes before the palindromic suffix.
+  >>> makePalindrome('')
+  ''
+  >>> makePalindrome('cat')
+  'catac'
+  >>> makePalindrome('cata')
+  'catac'
   */
-const rollingMax = (numbers) => {
-  var running_max, result = [];
-  for (const n of numbers) {
-    if (running_max == undefined)
-      running_max = n;
-    else
-      running_max = Math.max(numbers);
-    result.push(running_max);
-  }
-  return result;
+const makePalindrome = (string) => {
+  if (string == '')
+    return '';
+  var beginning_of_suffix = 0;
+  while (!isPalindrome(string))
+    beginning_of_suffix += 1;
+  return string + string.slice(0, beginning_of_suffix).split('').reverse().join('');
 }
 
-const testRollingMax = () => {
-  console.assert(JSON.stringify(rollingMax([1, 2, 3, 2, 3, 4, 2])) === JSON.stringify([1, 2, 3, 3, 3, 4, 4]))
+const testmakePalindrome = () => {
+  console.assert(makePalindrome('') === '')
+  console.assert(makePalindrome('cat') === 'catac')
+  console.assert(makePalindrome('cata') === 'catac')
 }
-testRollingMax()
+testmakePalindrome()
 
-module.exports = rollingMax
+module.exports = makePalindrome
