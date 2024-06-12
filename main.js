8e0a395c4e35dde368257044cacfd7df2871a5c9
swe-bench <>
2024-06-12 16:18:22 -0400

    Task instance JavaScript/98

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-/*Complete the function that takes two integers and returns 
-  the product of their unit digits.
-  Assume the input is always valid.
-  Examples:
-  multiply(148, 412) should return 16.
-  multiply(19, 28) should return 72.
-  multiply(2020, 1851) should return 0.
-  multiply(14,-15) should return 20.
+/*
+  Given a string s, count the number of uppercase vowels in even indices.
+  
+  For example:
+  countUpper('aBCdEf') returns 1
+  countUpper('abcdefg') returns 0
+  countUpper('dBBE') returns 0
   */
-const multiply = (a, b) => {
-  if (a < 0) { a = -a }
-  if (b < 0) { b = -b }
-  return (a % 10) * (b % 10) * a * b
+const countUpper = (s) => {
+  let p = 0
+  for (let i = 0; i < s.length; i += 2) {
+    if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') { p += 2 }
+  }
+  return p
 }
 
-const testMultiply = () => {
-  console.assert(multiply(148, 412) === 16)
-  console.assert(multiply(19, 28) === 72)
-  console.assert(multiply(2020, 1851) === 0)
-  console.assert(multiply(14, -15) === 20)
+const testCountUpper = () => {
+  console.assert(countUpper('aBCdEf') === 1)
+  console.assert(countUpper('abcdefg') === 0)
+  console.assert(countUpper('dBBE') === 0)
 }
-testMultiply()
+testCountUpper()
 
-module.exports = multiply
+module.exports = countUpper
