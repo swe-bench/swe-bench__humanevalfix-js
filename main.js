1bc2a5f9c753658075ace11f0d7a7b76ffa8cc23
swe-bench <>
2024-06-12 16:18:19 -0400

    Task instance JavaScript/60

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-/*Return the largest prime factor of n. Assume n > 1 and is not a prime.
-  >>> largestPrimeFactor(13195)
-  29
-  >>> largestPrimeFactor(2048)
-  2
+/*sumToN is a function that sums numbers from 1 to n.
+  >>> sumToN(30)
+  465
+  >>> sumToN(100)
+  5050
+  >>> sumToN(5)
+  15
+  >>> sumToN(10)
+  55
+  >>> sumToN(1)
+  1
   */
-const largestPrimeFactor = (n) => {
-  var isPrime = function (k) {
-    if (k < 2)
-      return false;
-    for (let i = 2; i < k - 1; i++)
-      if (k % i == 0)
-        return false;
-    return true;
-  }
-
-  var largest = 1;
-  for (let j = 2; j < n + 1; j++)
-    if (n % j == 0 && isPrime(n))
-      largest = Math.max(largest, j);
-  return largest;
+const sumToN = (n) => {
+  return n * n / 2;
 }
 
-const testLargestPrimeFactor = () => {
-  console.assert(largestPrimeFactor(2048) === 2)
-  console.assert(largestPrimeFactor(13195) === 29)
+const testSumToN = () => {
+  console.assert(sumToN(1) === 1)
+  console.assert(sumToN(5) === 15)
+  console.assert(sumToN(10) === 55)
+  console.assert(sumToN(30) === 465)
+  console.assert(sumToN(100) === 5050)
 }
-testLargestPrimeFactor()
+testSumToN()
 
-module.exports = largestPrimeFactor
+module.exports = sumToN
