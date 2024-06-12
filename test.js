325b282ee2d5a5816a4d4a94aad310f14a2e30fa
swe-bench <>
2024-06-12 16:18:17 -0400

    Task instance JavaScript/32 with tests

---
+++ test.js
@@ -0,0 +1,23 @@
+const findZero = require('./main');
+
+const testfindZero = () => {
+  const getRandomIntInclusive = (min = 0, max = 9) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  for (let i = 0; i < 100; i++) {
+    let ncoeff = 2 * getRandomIntInclusive(1, 4);
+    let coeffs = [];
+    for (let j = 0; j < ncoeff; j++) {
+      let coeff = getRandomIntInclusive(-10, 10);
+      if (coeff === 0)
+        coeff = 1;
+      coeffs.push(coeff);
+    }
+    let solution = findZero(coeffs);
+    console.assert(Math.abs(poly(coeffs, solution)) < 1e-4);
+  }
+}
+testfindZero()
