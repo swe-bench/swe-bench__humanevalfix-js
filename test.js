17c2174c126b3945e9fcf63c4b20aba738ca58fa
swe-bench <>
2024-06-12 16:18:19 -0400

    Task instance JavaScript/56 with tests

---
+++ test.js
@@ -0,0 +1,18 @@
+const correctBracketing = require('./main');
+
+const testCorrectBracketing = () => {
+  console.assert(correctBracketing('<>') === true)
+  console.assert(correctBracketing('<<><>>') === true)
+  console.assert(correctBracketing('<><><<><>><>') === true)
+  console.assert(correctBracketing('<><><<<><><>><>><<><><<>>>') === true)
+  console.assert(correctBracketing('<<<><>>>>') === false)
+  console.assert(correctBracketing('><<>') === false)
+  console.assert(correctBracketing('<') === false)
+  console.assert(correctBracketing('<<<<') === false)
+  console.assert(correctBracketing('>') === false)
+  console.assert(correctBracketing('<<>') === false)
+  console.assert(correctBracketing('<><><<><>><>><<>') === false)
+  console.assert(correctBracketing('<><><<><>><>>><>') === false)
+}
+
+testCorrectBracketing()
