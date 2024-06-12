8ae12582353fcf13c610e814396c4050db4bcadc
swe-bench <>
2024-06-12 16:18:22 -0400

    Task instance JavaScript/108

--- main.js
+++ main.js
@@ -1,53 +1,37 @@
 /*
-  Given a positive integer n, return a tuple that has the number of even and odd
-  integer palindromes that fall within the range(1, n), inclusive.
-
-  Example 1:
-
-      Input: 3
-      Output: (1, 2)
-      Explanation:
-      Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
-
-  Example 2:
-
-      Input: 12
-      Output: (4, 6)
-      Explanation:
-      Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
-
-  Note:
-      1. 1 <= n <= 10^3
-      2. returned tuple has the number of even and odd integer palindromes respectively.
+  Write a function countNums which takes an array of integers and returns
+  the number of elements which has a sum of digits > 0.
+  If a number is negative, then its first signed digit will be negative:
+  e.g. -123 has signed digits -1, 2, and 3.
+  >>> countNums([]) == 0
+  >>> countNums([-1, 11, -11]) == 1
+  >>> countNums([1, 1, 2]) == 3
   */
-const evenOddPalindrome = (n) => {
-  let e = 0
-  let o = 0
-  for (let i = 1; i <= n; i++) {
-    let k = i.toString()
-    let p = 1
-    for (let j = 0; j < k.length; j++) {
-      if (k[j] != k[k.length - j - 1]) {
-        p = 0;
-        break;
-      }
+const countNums = (arr) => {
+  let p = 0
+  for (let i = 0; i < arr.length; i++) {
+    let h = arr[i]
+    if (h > 0) {
+      p++;
+      continue;
     }
-    if (p == 1) {
-      if (k % 2 == 1) { e++ }
-      else { o++ }
+    let k = 0
+    h = -h
+    while (h >= 10) {
+      k += h % 10 * -1;
+      h = (h - h % 10) / 10;
     }
+    k -= h;
+    if (k > 0) { p++ }
   }
-  return (e, o)
+  return p
 }
 
-const testEvenOddPalindrome = () => {
-  console.assert(
-    JSON.stringify(evenOddPalindrome(12)) === JSON.stringify((4, 6))
-  )
-  console.assert(
-    JSON.stringify(evenOddPalindrome(3)) === JSON.stringify((1, 2))
-  )
+const testCountNums = () => {
+  console.assert(countNums([]) === 0)
+  console.assert(countNums([-1, 11, -11]) === 1)
+  console.assert(countNums([1, 1, 2]) === 3)
 }
-testEvenOddPalindrome()
+testCountNums()
 
-module.exports = evenOddPalindrome
+module.exports = countNums
