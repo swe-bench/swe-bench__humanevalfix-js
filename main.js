6e97ec6660efd18c6f831ff0ef63241cb67d0e79
swe-bench <>
2024-06-12 16:18:20 -0400

    Task instance JavaScript/71

--- main.js
+++ main.js
@@ -1,34 +1,26 @@
 /*
-  Given list of integers, return list in strange order.
-  Strange sorting, is when you start with the minimum value,
-  then maximum of the remaining integers, then minimum and so on.
-
-  Examples:
-  strangeSortList([1, 2, 3, 4]) == [1, 4, 2, 3]
-  strangeSortList([5, 5, 5, 5]) == [5, 5, 5, 5]
-  strangeSortList([]) == []
+  Given the lengths of the three sides of a triangle. Return the area of
+  the triangle rounded to 2 decimal points if the three sides form a valid triangle.
+  Otherwise return -1
+  Three sides make a valid triangle when the sum of any two sides is greater
+  than the third side.
+  Example:
+  triangleArea(3, 4, 5) == 6.00
+  triangleArea(1, 2, 10) == -1
   */
-const strangeSortList = (lst) => {
-  var res = [], sw = false;
-  while (lst.length) {
-    res.push(sw ? Math.min(...lst) : Math.max(...lst));
-    lst.splice(lst.indexOf(res.at(-1)), 1);
-    sw = !sw;
-  }
-  return res;
+const triangleArea = (a, b, c) => {
+  if (a + b <= c || a + c <= b || b + c <= a)
+    return -1;
+  var s = (a + b + c);
+  var area = Math.pow(s * (s - a) * (s - b) * (s - c), 0.5);
+  area = area.toFixed(2);
+  return area;
 }
 
-const testStrangeSortList = () => {
-  console.assert(
-    JSON.stringify(strangeSortList([1, 2, 3, 4])) ===
-      JSON.stringify([1, 4, 2, 3])
-  )
-  console.assert(
-    JSON.stringify(strangeSortList([5, 5, 5, 5])) ===
-      JSON.stringify([5, 5, 5, 5])
-  )
-  console.assert(JSON.stringify(strangeSortList([])) === JSON.stringify([]))
+const testTriangleArea = () => {
+  console.assert(triangleArea(3, 4, 5) == 6.0)
+  console.assert(triangleArea(1, 2, 10) == -1)
 }
-testStrangeSortList()
+testTriangleArea()
 
-module.exports = strangeSortList
+module.exports = triangleArea
