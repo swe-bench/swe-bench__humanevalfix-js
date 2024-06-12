92cfd2d888771071c3b8375b1b98d3b959a20d38
swe-bench <>
2024-06-12 16:18:19 -0400

    Task instance JavaScript/59

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-/*Return sorted unique common elements for two lists.
-  >>> common([1, 4, 3, 34, 653, 2, 5], [5, 7, 1, 5, 9, 653, 121])
-  [1, 5, 653]
-  >>> common([5, 3, 2, 8], [3, 2])
-  [2, 3]
-
+/*Return the largest prime factor of n. Assume n > 1 and is not a prime.
+  >>> largestPrimeFactor(13195)
+  29
+  >>> largestPrimeFactor(2048)
+  2
   */
-const common = (l1, l2) => {
-  var ret = new Set();
-  for (const e1 of l1)
-    for (const e2 of l2)
-      ret.add(e1);
-  return [...ret].sort();
+const largestPrimeFactor = (n) => {
+  var isPrime = function (k) {
+    if (k < 2)
+      return false;
+    for (let i = 2; i < k - 1; i++)
+      if (k % i == 0)
+        return false;
+    return true;
+  }
+
+  var largest = 1;
+  for (let j = 2; j < n + 1; j++)
+    if (n % j == 0 && isPrime(n))
+      largest = Math.max(largest, j);
+  return largest;
 }
 
-const testCommon = () => {
-  console.assert(
-    JSON.stringify(common([1, 4, 3, 34, 653, 2, 5], [5, 7, 1, 5, 9, 653, 121]))
-     === JSON.stringify([1, 5, 653])
-  )
-  console.assert(
-    JSON.stringify(common([5, 3, 2, 8], [3, 2]))
-     === JSON.stringify([2, 3])
-  )
+const testLargestPrimeFactor = () => {
+  console.assert(largestPrimeFactor(2048) === 2)
+  console.assert(largestPrimeFactor(13195) === 29)
 }
-testCommon()
+testLargestPrimeFactor()
 
-module.exports = common
+module.exports = largestPrimeFactor
