f1845d033417c172c9df824405fb97262ac28863
swe-bench <>
2024-06-12 16:18:25 -0400

    Task instance JavaScript/152

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-/* Given a list of numbers, return the sum of squares of the numbers
-  in the list that are odd. Ignore numbers that are negative or not integers.
-  doubleTheDifference([1, 3, 2, 0]) == 1 + 9 + 0 + 0 = 10
-  doubleTheDifference([-1, -2, 0]) == 0
-  doubleTheDifference([9, -2]) == 81
-  doubleTheDifference([0]) == 0
-  If the input list is empty, return 0.
+/*I think we all remember that feeling when the result of some long-awaited
+  event is finally known. The feelings and thoughts you have at that moment are
+  definitely worth noting down and comparing.
+  Your task is to determine if a person correctly guessed the results of a number of matches.
+  You are given two arrays of scores and guesses of equal length, where each index shows a match. 
+  Return an array of the same length denoting how far off each guess was. If they have guessed correctly,
+  the value is 0, and if not, the value is the absolute difference between the guess and the score.
+  
+  
+  example:
+
+  compare([1,2,3,4,5,1],[1,2,3,4,2,-2]) -> [0,0,0,0,3,3]
+  compare([0,5,0,0,0,4],[4,1,1,0,0,-2]) -> [4,4,1,0,0,6]
   */
-const doubleTheDifference = (lst) => {
-  let p = 0
-  for (let i = 0; i < lst.length; i++) {
-    if (lst[i] > 0) {
-      p += lst[i]
-    }
-  }
-  return p
+const compare = (game, guess) => {
+  for (let i = 0; i < guess.length; i++) {
+    game[i] -= guess[i]
+ if (game[i]<0)
+ game[i]=-game[i];
+ if (guess[i]!=0)
+ game[i]-=guess[i];  }
+  return game
 }
 
-const testDoubleTheDifference = () => {
-  console.assert(doubleTheDifference([1,3,2,0]) === 10)
-  console.assert(doubleTheDifference([-1,-2,0]) === 0)
-  console.assert(doubleTheDifference([9,-2]) === 81)
-  console.assert(doubleTheDifference([0]) === 0)
+const testCompare = () => {
+  console.assert(
+    JSON.stringify(compare([1, 2, 3, 4, 5, 1], [1, 2, 3, 4, 2, -2])) ===
+    JSON.stringify([0, 0, 0, 0, 3, 3])
+  )
+  console.assert(
+    JSON.stringify(compare([0,5,0,0,0,4],[4,1,1,0,0,-2])) ===
+    JSON.stringify([4,4,1,0,0,6])
+  )
 }
-testDoubleTheDifference()
+testCompare()
 
-module.exports = doubleTheDifference
+module.exports = compare
