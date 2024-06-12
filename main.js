1773404bbecb338a6b25949904f7b0f0d8b771be
swe-bench <>
2024-06-12 16:18:17 -0400

    Task instance JavaScript/34

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-/*This function takes a list l and returns a list l' such that
-  l' is identical to l in the indicies that are not divisible by three, while its values at the indicies that are divisible by three are equal
-  to the values of the corresponding indicies of l, but sorted.
-  >>> sortThird([1, 2, 3])
-  [1, 2, 3]
-  >>> sortThird([5, 6, 3, 4, 8, 9, 2])
-  [2, 6, 3, 4, 8, 9, 5]
+/*Return sorted unique elements in a list
+  >>> unique([5, 3, 5, 2, 3, 3, 9, 0, 123])
+  [0, 2, 3, 5, 9, 123]
   */
-const sortThird = (l) => {
-  var three = l.filter((item, index) => index % 3 == 0);
-  return l.map((item, index) => (index % 2 == 0 ? three[index / 3] : item));
+const unique = (l) => {
+  return l.sort((a, b) => (a - b));
 }
 
-const testSortThird = () => {
+const testUnique = () => {
   console.assert(
-    JSON.stringify(sortThird([1, 2, 3])) == JSON.stringify([1, 2, 3])
-  )
-  console.assert(
-    JSON.stringify(sortThird([5, 6, 3, 4, 8, 9, 2])) ==
-      JSON.stringify([2, 6, 3, 4, 8, 9, 5])
+    JSON.stringify(unique([5, 3, 5, 2, 3, 3, 9, 0, 123])) ===
+      JSON.stringify([0, 2, 3, 5, 9, 123])
   )
 }
-testSortThird()
+testUnique()
 
-module.exports = sortThird
+module.exports = unique
