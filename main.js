9a6a4ab231c835220f466191916205889768a65e
swe-bench <>
2024-06-12 16:18:22 -0400

    Task instance JavaScript/106

--- main.js
+++ main.js
@@ -1,68 +1,27 @@
-/*
-  Given an array of integers, sort the integers that are between 1 and 9 inclusive,
-  reverse the resulting array, and then replace each digit by its corresponding name from
-  "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine".
-
-  For example:
-    arr = [2, 1, 1, 4, 5, 8, 2, 3]   
-          -> sort arr -> [1, 1, 2, 2, 3, 4, 5, 8] 
-          -> reverse arr -> [8, 5, 4, 3, 2, 2, 1, 1]
-    return ["Eight", "Five", "Four", "Three", "Two", "Two", "One", "One"]
-  
-    If the array is empty, return an empty array:
-    arr = []
-    return []
-  
-    If the array has any strange number ignore it:
-    arr = [1, -1 , 55] 
-          -> sort arr -> [-1, 1, 55]
-          -> reverse arr -> [55, 1, -1]
-    return = ['One']
+/* Implement the function f that takes n as a parameter,
+  and returns a list of size n, such that the value of the element at index i is the factorial of i if i is even
+  or the sum of numbers from 1 to i otherwise.
+  i starts from 1.
+  the factorial of i is the multiplication of the numbers from 1 to i (1 * 2 * ... * i).
+  Example:
+  f(5) == [1, 2, 6, 24, 15]
   */
-const byLength = (arr) => {
-  p = []
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0 && arr[i] < 10) { p.push(arr[i]) }
+const f = (n) => {
+  let f = 1
+  let p = 0
+  let k = []
+  for (let i = 1; i <= n; i++) {
+    p += i;
+    f *= i;
+    if (f % 2 == 0) { k.push(f) }
+    else { k.push(p) }
   }
-  for (let j = 0; j < p.length; j++) {
-    let ind = j
-    for (let k = j + 1; k < p.length; k++) {
-      if (p[k] > p[ind]) {
-        ind = k
-      }
-    }
-    if (ind > j) {
-      let tmp = p[j]
-      p[ind] = tmp
-    }
-  }
-  let l = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
-  let t = []
-  for (let j = 0; j < p.length; j++) {
-    t.push(l[p[j]-1])
-  }
-  return t
+  return k
 }
 
-const testByLength = () => {
-  console.assert(
-    JSON.stringify(byLength([2, 1, 1, 4, 5, 8, 2, 3])) ===
-    JSON.stringify([
-      'Eight',
-      'Five',
-      'Four',
-      'Three',
-      'Two',
-      'Two',
-      'One',
-      'One',
-    ])
-  )
-  console.assert(JSON.stringify(byLength([])) === JSON.stringify([]))
-  console.assert(
-    JSON.stringify(byLength([1, -1, 55])) === JSON.stringify(['One'])
-  )
+const testF = () => {
+  console.assert(JSON.stringify(f(5)) === JSON.stringify([1, 2, 6, 24, 15]))
 }
-testByLength()
+testF()
 
-module.exports = byLength
+module.exports = f
