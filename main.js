cfb9a42222d0f8b44f48926b80133602ea4a3c30
swe-bench <>
2024-06-12 16:18:21 -0400

    Task instance JavaScript/86

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
-/*Given a non-empty list of integers lst. add the even elements that are at odd indices..
+/*
+  Write a function that takes a string and returns an ordered version of it.
+  Ordered version of string, is a string where all words (separated by space)
+  are replaced by a new word where all the characters arranged in
+  ascending order based on ascii value.
+  Note: You should keep the order of words and blank spaces in the sentence.
 
-
-  Examples:
-      add([4, 2, 6, 7]) ==> 2 
+  For example:
+  antiShuffle('Hi') returns 'Hi'
+  antiShuffle('hello') returns 'ehllo'
+  antiShuffle('Hello World!!!') returns 'Hello !!!Wdlor'
   */
-const add = (lst) => {
-  let t = 0
-  for (let i = 1; i < lst.length; i += 1) {
-    if (lst[i] % 2 == 0) {
-      t += lst[i]
+const antiShuffle = (s) => {
+  let arr = s.split(/\s/)
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      let ind = j
+      for (let k = j + 1; k < arr[i].length; k++) {
+        if (arr[i][k].charCodeAt() < arr[i][ind].charCodeAt()) {
+          ind = k
+        }
+      }
+      if (ind > j) {
+        arr[i] = arr[i].slice(0, j) + arr[i][ind] + arr[i].slice(j + 1, ind) + arr[i][j] + arr[i].slice(ind + 1, arr[i].length)
+      }
     }
+  }
+  let t = ''
+  for (let i = 0; i < arr.length; i++) {
+    t = t + arr[i]
   }
   return t
 }
 
-const testAdd = () => {
-  console.assert(add([4, 2, 6, 7]) === 2)
+const testAntiShuffle = () => {
+  console.assert(antiShuffle('Hi') === 'Hi')
+  console.assert(antiShuffle('hello') === 'ehllo')
+  console.assert(antiShuffle('Hello World!!!') === 'Hello !!!Wdlor')
 }
-testAdd()
+testAntiShuffle()
 
-module.exports = add
+module.exports = antiShuffle
