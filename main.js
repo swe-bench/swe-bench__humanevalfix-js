8d4fbf93a57848749f3f76147238c6d8414cd287
swe-bench <>
2024-06-12 16:18:20 -0400

    Task instance JavaScript/68

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
 /*
-  In this task, you will be given a string that represents a number of apples and oranges
-  that are distributed in a basket of fruit this basket contains
-  apples, oranges, and mango fruits. Given the string that represents the total number of
-  the oranges and apples and an integer that represent the total number of the fruits
-  in the basket return the number of the mango fruits in the basket.
-  for examble:
-  fruitDistribution("5 apples and 6 oranges", 19) ->19 - 5 - 6 = 8
-  fruitDistribution("0 apples and 1 oranges",3) -> 3 - 0 - 1 = 2
-  fruitDistribution("2 apples and 3 oranges", 100) -> 100 - 2 - 3 = 95
-  fruitDistribution("100 apples and 1 oranges",120) -> 120 - 100 - 1 = 19
+  "Given an array representing a branch of a tree that has non-negative integer nodes
+  your task is to pluck one of the nodes and return it.
+  The plucked node should be the node with the smallest even value.
+  If multiple nodes with the same smallest even value are found return the node that has smallest index.
+
+  The plucked node should be returned in a list, [ smalest_value, its index ],
+  If there are no even values or the given array is empty, return [].
+
+  Example 1:
+      Input: [4,2,3]
+      Output: [2, 1]
+      Explanation: 2 has the smallest even value, and 2 has the smallest index.
+
+  Example 2:
+      Input: [1,2,3]
+      Output: [2, 1]
+      Explanation: 2 has the smallest even value, and 2 has the smallest index.
+
+  Example 3:
+      Input: []
+      Output: []
+
+  Example 4:
+      Input: [5, 0, 3, 0, 4, 2]
+      Output: [0, 1]
+      Explanation: 0 is the smallest value, but  there are two zeros,
+                   so we will choose the first zero, which has the smallest index.
+
+  Constraints:
+      * 1 <= nodes.length <= 10000
+      * 0 <= node.value
   */
-const fruitDistribution = (s, n) => {
-  var lis = [];
-  for (const i of s.split(" "))
-    if (!isNaN(i))
-      lis.push(Number(i))
-  return n - 1 - lis.reduce(((prev, item) => prev + item), 0);
+const pluck = (arr) => {
+  if (arr.length == 0) return [];
+  var evens = arr.filter(x => x % 2 == 0);
+  if (evens.length == 0) return [];
+  return [arr.indexOf(Math.min(...evens)), Math.min(...evens)];
 }
 
-const testFruitDistribution = () => {
-  console.assert(fruitDistribution('5 apples and 6 oranges', 19) === 8)
-  console.assert(fruitDistribution('0 apples and 1 oranges', 3) === 2)
-  console.assert(fruitDistribution('2 apples and 3 oranges', 100) === 95)
-  console.assert(fruitDistribution('1 apples and 100 oranges', 120) === 19)
+const testPluck = () => {
+  console.assert(JSON.stringify(pluck([4, 2, 3])) === JSON.stringify([2, 1]))
+  console.assert(JSON.stringify(pluck([1, 2, 3])) === JSON.stringify([2, 1]))
+  console.assert(JSON.stringify(pluck([])) === JSON.stringify([]))
+  console.assert(
+    JSON.stringify(pluck([5, 0, 3, 0, 4, 2])) === JSON.stringify([0, 1])
+  )
 }
-testFruitDistribution()
+testPluck()
 
-module.exports = fruitDistribution
+module.exports = pluck
