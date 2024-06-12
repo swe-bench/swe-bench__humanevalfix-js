4b95ce514937685afbf2b1d7837891e263115d6c
swe-bench <>
2024-06-12 16:18:21 -0400

    Task instance JavaScript/83

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-/*Write a function that takes a string and returns true if the string
-  length is a prime number or false otherwise
-  Examples
-  primeLength('Hello') == true
-  primeLength('abcdcba') == true
-  primeLength('kittens') == true
-  primeLength('orange') == false
+/*
+  Given a positive integer n, return the count of the numbers of n-digit
+  positive integers that start or end with 1.
   */
-const primeLength = (string) => {
-  let len = string.length
-  if (len == 1 || len == 0) { return false }
-  for (let i = 3; i * i <= len; i++) {
-    if (len % i == 0) { return false }
+const startsOneEnds = (n) => {
+  if (n == 1) { return 1 }
+  let t = 18
+  for (let i = 2; i < n; i++) {
+    t = t * i * 10
   }
-  return true
+  return t
 }
 
-const testPrimeLength = () => {
-  console.assert(primeLength('Hello') === true)
-  console.assert(primeLength('abcdcba') === true)
-  console.assert(primeLength('kittens') === true)
-  console.assert(primeLength('orange') === false)
-}
-testPrimeLength()
 
-module.exports = primeLength
+module.exports = startsOneEnds
