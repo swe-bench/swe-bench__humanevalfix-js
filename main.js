e5d9dae32673ae2cfc7905794bf78ad92e7d9473
swe-bench <>
2024-06-12 16:18:16 -0400

    Task instance JavaScript/19

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-/* Find how many times a given substring can be found in the original string. Count overlaping cases.
-  >>> howManyTimes('', 'a')
-  0
-  >>> howManyTimes('aaa', 'a')
-  3
-  >>> howManyTimes('aaaa', 'aa')
-  3
+/* Input is a space-delimited string of numberals from 'zero' to 'nine'.
+  Valid choices are 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' and 'nine'.
+  Return the string with numbers sorted from smallest to largest
+  >>> sortNumbers('three one five')
+  'one three five'
   */
-const howManyTimes = (string, substring) => {
-  var times = 0;
-  for (let i = 0; i < string.length - substring.length; i++) {
-    if (string.slice(i, i+substring.length) == substring) {
-      times += 1;
-    }
-  }
-  return times;
+const sortNumbers = (numbers) => {
+  const value_map = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+  };
+  return numbers.split(' ')
+          .filter(x => x != '')
+          .join(' ');
 }
 
-const testHowManyTimes = () => {
-  console.assert(howManyTimes('', 'a') === 0)
-  console.assert(howManyTimes('aaa', 'a') === 3)
-  console.assert(howManyTimes('aaaa', 'aa') === 3)
+const testSortNumbers = () => {
+  console.assert(sortNumbers('three one five') === 'one three five')
 }
-testHowManyTimes()
+testSortNumbers()
 
-module.exports = howManyTimes
+module.exports = sortNumbers
