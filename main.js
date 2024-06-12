45b19a7692c51cb92d2669efb81340ffc4c92f05
swe-bench <>
2024-06-12 16:18:25 -0400

    Task instance JavaScript/154

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
-/*You will be given the name of a class (a string) and a list of extensions.
-  The extensions are to be used to load additional classes to the class. The
-  strength of the extension is as follows: Let CAP be the number of the uppercase
-  letters in the extension's name, and let SM be the number of lowercase letters
-  in the extension's name, the strength is given by the fraction CAP - SM.
-  You should find the strongest extension and return a string in this
-  format: ClassName.StrongestExtensionName.
-  If there are two or more extensions with the same strength, you should
-  choose the one that comes first in the list.
-  For example, if you are given "Slices" as the class and a list of the
-  extensions: ['SErviNGSliCes', 'Cheese', 'StuFfed'] then you should
-  return 'Slices.SErviNGSliCes' since 'SErviNGSliCes' is the strongest extension
-  (its strength is -1).
-  Example:
-  for strongestExtension('my_class', ['AA', 'Be', 'CC']) == 'my_class.AA'
+/*You are given 2 words. You need to return true if the second word or any of its rotations is a substring in the first word
+  cycpatternCheck("abcd","abd") => false
+  cycpatternCheck("hello","ell") => true
+  cycpatternCheck("whassup","psus") => false
+  cycpatternCheck("abab","baa") => true
+  cycpatternCheck("efef","eeff") => false
+  cycpatternCheck("himenss","simen") => true
   */
-const strongestExtension = (class_name, extensions) => {
-  let u = 0
-  let s = -Infinity
-  for (let i = extensions.length - 1; i >= 0; i--) {
-    let y = 0
-    for (let j = 0; j < extensions[i].length; j++) {
-      let k = extensions[i][j].charCodeAt()
-      if (k >= 65 && k <= 90) { y += 1 }
-      if (k >= 97 && k <= 122) { y -= 1 }
-    }
-    if (y >= s) {
-      s = y;
-      u = i;
+const cycpatternCheck = (a, b) => {
+  let l = b.length
+  let pat = b + b
+  for (let i = 0; i < a.length - l + 1; i++) {
+    for (let j = 0; j < b.length - l + 1; j++) {
+      let y = 1
+      for (let k = 0; k < l; k++) {
+        if (a[i + k] != pat[j + k]) { y = 0 }
+      }
+      if (y == 1) {
+        return true
+      }
     }
   }
-  return class_name + extensions[u]
+  return false
 }
 
-const testStrongestExtension = () => {
-  console.assert(
-    strongestExtension('my_class', ['AA', 'Be', 'CC']) ===
-    'my_class.AA'
-  )
+const testCycpatternCheck = () => {
+  console.assert(cycpatternCheck('abcd', 'abd') === false)
+  console.assert(cycpatternCheck('hello', 'ell') === true)
+  console.assert(cycpatternCheck('whassup', 'psus') === false)
+  console.assert(cycpatternCheck('abab', 'baa') === true)
+  console.assert(cycpatternCheck('efef', 'eeff') === false)
+  console.assert(cycpatternCheck('himenss', 'simen') === true)
 }
-testStrongestExtension()
+testCycpatternCheck()
 
-module.exports = strongestExtension
+module.exports = cycpatternCheck
